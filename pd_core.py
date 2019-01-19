f4b1948110d8d1209e2a26b146cfeea65351bbc1
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-01-19 19:09:22 +0530

    Add frequency handler

--- pd_core.py
+++ pd_core.py
@@ -15,24 +15,30 @@ def get_incident_frequency(**kwargs):
 	api_key = config[team]["API_KEY"]
 
 	incidents = get_incidents(since=since, until=until, team_id=team_id, urgencies=urgencies, api_key=api_key)
-
 	incident_frequency = {}
+	incident_ids = {}
 
 	for incident in incidents:
 		title = incident.get("title")
 		incident_frequency[title] = incident_frequency.get(title, 0) + 1
+		if not incident_ids.get(title):
+			incident_ids[title] = incident.get("id")
 
 	incident_frequency_list = []
 
 	for incident in incident_frequency:
 		incident_frequency_list.append({
 			"incident_name": incident,
+			"incident_id": incident_ids.get(incident),
 			"incident_frequency": incident_frequency[incident]
 		})
 
+	incident_frequency_list.sort(key=lambda x:x.get("incident_frequency"))
+	incident_frequency_list.reverse()
+
 	response = {
 		"unique_incident_count": len(incident_frequency_list),
-		"incident_frequency": incident_frequency_list,
+		"incident_frequencies": incident_frequency_list,
 		"since": since,
 		"until": until,
 		"urgency": urgencies,
@@ -42,4 +48,8 @@ def get_incident_frequency(**kwargs):
 	return response
 
 if __name__ == "__main__":
-	pass
+	r = get_incident_frequency(since="18-01-2019", until="19-01-2019", team="CM", team_id='PAE6H7O')
+	print ("\n")
+	print (r["incident_frequencies"][0])
+	print ("\n")
+	print (r["incident_frequencies"][1])

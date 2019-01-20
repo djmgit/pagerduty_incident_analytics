2e4be250a0f400d21393c403219649b0d04e3dd1
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-01-20 20:07:32 +0530

    Add UI

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,13 @@
 // Iniialise app and controller
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-  $scope.name = "hello";
+	
 
-  // initialise
+ 
+
+  // /$scope.initialise();
 });
+
+
 
 

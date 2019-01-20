995148f17f3ce3a786b76e25279f14441af9d267
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-01-20 23:55:21 +0530

    Update UI method handlers

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,29 @@
 // Iniialise app and controller
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
+app.controller('myCtrl', function($scope, $http) {
+	
+	$scope.click = function() {
+		console.log($scope.team);
+	}
+
+	$scope.init = function() {
+
+		$http.get("/api/v1/teams").then(function(response) {
+			$scope.teams = response.data.teams;
+			console.log($scope.teams);
+		})
+	}
+
+	$scope.fetch = function() {
+		
+	}
+
+
+	$scope.init();
 	
 
  
 
-  // /$scope.initialise();
 });
 
 
